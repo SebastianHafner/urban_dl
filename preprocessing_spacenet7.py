78cf7be72e406cc667edd392d35feeee5d4f612d
SebastianHafner <hafnersailing@gmail.com>
2020-09-11 09:32:28 +0200

    implemented bbox extraction

--- preprocessing_spacenet7.py
+++ preprocessing_spacenet7.py
@@ -2,8 +2,10 @@ from pathlib import Path
 from utils.geotiff import *
 import json
 import ee
+import utm
+import pandas as pd
 
-SPACENET7_PATH = Path('C:/Users/shafner/urban_extraction/data/spacenet7/train')
+SPACENET7_PATH = Path('C:/Users/hafne/urban_extraction/data/spacenet7/train')
 
 
 def extract_bbox(aoi_name: str):
@@ -14,9 +16,6 @@ def extract_bbox(aoi_name: str):
     img_file = all_img_files[0]
     arr, transform, crs = read_tif(img_file)
     y_pixels, x_pixels, _ = arr.shape
-    print(y_pixels, x_pixels)
-    print(crs)
-    print(transform)
 
     x_pixel_spacing = transform[0]
     x_min = transform[2]
@@ -26,23 +25,51 @@ def extract_bbox(aoi_name: str):
     y_max = transform[5]
     y_min = y_max + y_pixels * y_pixel_spacing
 
-    print(f'x min: {x_min:.3f} - x max: {x_max:.3f}')
-    print(f'y min: {y_min:.3f} - y max: {y_max:.3f}')
+    bbox = ee.Geometry.Rectangle([x_min, y_min, x_max, y_max], proj=str(crs)).transform('EPSG:4326')
+    return bbox
 
-    ul = ee.Geometry.Point([50, 50])
-    print(ul.getInfo())
-    bbox = ee.Geometry.Rectangle([x_min, y_min, x_max, y_max], proj=str(crs))
-    print(bbox.getInfo())
-    data = ee.FeatureCollection([bbox]).getInfo()
 
-    bbox_file = SPACENET7_PATH.parent / 'bboxes' / f'bbox_{aoi_name}.geojson'
-    with open(str(bbox_file), 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+def epsg_utm(bbox):
+    center_point = bbox.centroid()
+    coords = center_point.getInfo()['coordinates']
+    lon, lat = coords
+    easting, northing, zone_number, zone_letter = utm.from_latlon(lat, lon)
+    return f'EPSG:326{zone_number}' if lat > 0 else f'EPSG:327{zone_number}'
 
 
 if __name__ == '__main__':
 
     ee.Initialize()
-    aoi_name = 'L15-0331E-1257N_1327_3160_13'
 
-    extract_bbox(aoi_name)
+    metadata_file = SPACENET7_PATH.parent / 'sn7_metadata.csv'
+    mdf = pd.read_csv(metadata_file)
+
+    aoi_names = [f.name for f in SPACENET7_PATH.iterdir() if f.is_dir()]
+    patch_features = []
+    point_features = []
+    for i, aoi_id in enumerate(aoi_names):
+        row = mdf.loc[mdf['aoi_id'] == aoi_id]
+        year = int(row['year'])
+        month = int(row['month'])
+        group = int(row['group'])
+        bbox = extract_bbox(aoi_id)
+        epsg = epsg_utm(bbox)
+        properties = {
+            'aoi': aoi_id,
+            'epsg': epsg,
+            'year': year,
+            'month': month,
+            'group': group
+        }
+        patch_features.append(ee.Feature(bbox, properties))
+        point_features.append(ee.Feature(bbox.centroid(), properties))
+
+    patch_data = ee.FeatureCollection(patch_features).getInfo()
+    patch_file = SPACENET7_PATH.parent / f'sn7_patches.geojson'
+    with open(str(patch_file), 'w', encoding='utf-8') as f:
+        json.dump(patch_data, f, ensure_ascii=False, indent=4)
+
+    points_data = ee.FeatureCollection(point_features).getInfo()
+    points_file = SPACENET7_PATH.parent / f'sn7_points.geojson'
+    with open(str(points_file), 'w', encoding='utf-8') as f:
+        json.dump(points_data, f, ensure_ascii=False, indent=4)

fixed accuracy metric computation and improved testing

quantitative_assessment.py
@@ -14,13 +14,13 @@ CONFIG_PATH = Path('/home/shafner/urban_dl/configs')
 NETWORK_PATH = Path('/storage/shafner/urban_extraction/networks/')
 
 
-def compute_accuracy_metrics(config_name: str, output_file: Path):
+def compute_accuracy_metrics(config_name: str, checkpoint: int, output_file: Path = None):
 
     cfg_file = CONFIG_PATH / f'{config_name}.yaml'
     cfg = config.load_cfg(cfg_file)
 
     # loading network
-    net_file = NETWORK_PATH / f'{config_name}.pkl'
+    net_file = NETWORK_PATH / f'{config_name}_{checkpoint}.pkl'
     net = load_network(cfg, net_file)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     net.to(device)
@@ -58,8 +58,9 @@ def compute_accuracy_metrics(config_name: str, output_file: Path):
 
             data[run_type][site] = {'f1_score': f1.item(), 'precision': prec.item(), 'recall': rec.item()}
 
-    with open(str(output_file), 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    if output_file is not None:
+        with open(str(output_file), 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 def plot_quantitative_results(files: list, names: list, run_type: str):
@@ -92,11 +93,12 @@ def plot_quantitative_results(files: list, names: list, run_type: str):
 
 if __name__ == '__main__':
     config_name = 'baseline_fusion'
+    checkpoint = 100
     output_file = DATASET_PATH.parent / 'quantitative_assessment' / f'qantitative_assessment_{config_name}.json'
-    # compute_accuracy_metrics(config_name, output_file)
+    compute_accuracy_metrics(config_name, checkpoint)
 
-
-    fusion_file = DATASET_PATH.parent / 'quantitative_assessment' / f'qantitative_assessment_baseline_fusion.json'
-    optical_file = DATASET_PATH.parent / 'quantitative_assessment' / f'qantitative_assessment_baseline_optical.json'
-    sar_file = DATASET_PATH.parent / 'quantitative_assessment' / f'qantitative_assessment_baseline_sar.json'
-    plot_quantitative_results([sar_file, optical_file, fusion_file], ['SAR', 'optical', 'fusion'], 'validation')
+    # TODO: only pass config name to function
+    # fusion_file = DATASET_PATH.parent / 'quantitative_assessment' / f'qantitative_assessment_baseline_fusion.json'
+    # optical_file = DATASET_PATH.parent / 'quantitative_assessment' / f'qantitative_assessment_baseline_optical.json'
+    # sar_file = DATASET_PATH.parent / 'quantitative_assessment' / f'qantitative_assessment_baseline_sar.json'
+    # plot_quantitative_results([sar_file, optical_file, fusion_file], ['SAR', 'optical', 'fusion'], 'validation')

testing.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import numpy as np
 import json
 import torch
+from tqdm import tqdm
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from networks.network_loader import load_network
 from utils.dataloader import SpaceNet7Dataset
 from experiment_manager.config import config
@@ -13,6 +16,9 @@ DATASET_PATH = Path('/storage/shafner/urban_extraction/urban_extraction')
 CONFIG_PATH = Path('/home/shafner/urban_dl/configs')
 NETWORK_PATH = Path('/storage/shafner/urban_extraction/networks/')
 
+GROUPS = [(1, 'NA_AU', '#63cd93'), (2, 'SA', '#f0828f'), (3, 'EU', '#6faec9'), (4, 'SSA', '#5f4ad9'),
+          (5, 'NAF_ME', '#8dee47'), (6, 'AS', '#d9b657'), ('total', 'Total', '#ffffff')]
+
 
 def qualitative_testing(config_name: str, checkpoint: int, threshold: int = None):
     cfg_file = CONFIG_PATH / f'{config_name}.yaml'
@@ -22,14 +28,14 @@ def qualitative_testing(config_name: str, checkpoint: int, threshold: int = None
     dataset = SpaceNet7Dataset(cfg)
 
     # loading network
-    net_file = NETWORK_PATH / f'{config_name}.pkl'
+    net_file = NETWORK_PATH / f'{config_name}_{checkpoint}.pkl'
     net = load_network(cfg, net_file)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     net.to(device)
     net.eval()
     thresh = cfg.THRESH if threshold is None else threshold
 
-    for index in range(len(dataset)):
+    for index in tqdm(range(len(dataset))):
         sample = dataset.__getitem__(index)
         aoi_id = sample['aoi_id']
 
@@ -81,12 +87,11 @@ def quantitative_testing(config_name: str, checkpoint: int, save_output: bool = 
     # container for output file
     output_data = {
         'metadata': {'cfg_name': config_name, 'thresh': thresh},
-        'groups': [(1, 'NA_AU', '#63cd93'), (2, 'SA', '#f0828f'), (3, 'EU', '#6faec9'), (4, 'SSA', '#5f4ad9'),
-                   (5, 'NAF_ME', '#8dee47'), (6, 'AS', '#d9b657'), ('total', 'Total', '#ffffff')],
+        'groups': GROUPS,
         'data': {}
     }
 
-    for index in range(len(dataset)):
+    for index in tqdm(range(len(dataset))):
         sample = dataset.__getitem__(index)
         group = sample['group']
 
@@ -109,16 +114,16 @@ def quantitative_testing(config_name: str, checkpoint: int, save_output: bool = 
             y_true_dict['total'] = np.concatenate((y_true_dict['total'], y_true))
             y_pred_dict['total'] = np.concatenate((y_pred_dict['total'], y_pred))
 
-    # TODO: improve order
-    for group in y_true_dict.keys():
-        group_y_true = torch.Tensor(np.array(y_true_dict[group]))
-        group_y_pred = torch.Tensor(np.array(y_pred_dict[group]))
+    for group in GROUPS:
+        group_index, group_name, _ = group
+        group_y_true = torch.Tensor(np.array(y_true_dict[group_index]))
+        group_y_pred = torch.Tensor(np.array(y_pred_dict[group_index]))
         prec = precision(group_y_true, group_y_pred, dim=0).item()
         rec = recall(group_y_true, group_y_pred, dim=0).item()
         f1 = f1_score(group_y_true, group_y_pred, dim=0).item()
 
-        output_data['data'][group] = {'f1_score': f1, 'precision': prec, 'recall': rec}
-        print(f'Group {group} - Precision: {prec:.3f} - Recall: {rec:.3f} - F1 score: {f1:.3f}')
+        output_data['data'][group_index] = {'f1_score': f1, 'precision': prec, 'recall': rec}
+        print(f'{group_name} ({group_index}) - Precision: {prec:.3f} - Recall: {rec:.3f} - F1 score: {f1:.3f}')
 
     if save_output:
         output_file = DATASET_PATH.parent / 'testing' / f'testing_{config_name}.json'
@@ -158,7 +163,7 @@ def qualitative_testing_comparison(config_names: list, checkpoint: int):
     # setup
     configs = [config.load_cfg(CONFIG_PATH / f'{config_name}.yaml') for config_name in config_names]
     datasets = [SpaceNet7Dataset(cfg) for cfg in configs]
-    net_files = [NETWORK_PATH / f'{config_name}.pkl' for config_name in config_names]
+    net_files = [NETWORK_PATH / f'{config_name}_{checkpoint}.pkl' for config_name in config_names]
 
     # optical, sar, reference and predictions (n configs)
     n_plots = 3 + len(config_names)
@@ -178,8 +183,10 @@ def qualitative_testing_comparison(config_names: list, checkpoint: int):
             group_name = sample['group_name']
 
             if i == 0:
-                super_title = f'{country} ({group_name})'
-                fig.suptitle(super_title)
+                super_title = f'{aoi_id} ({country},  {group_name})'
+                fig.suptitle(super_title, size=20)
+                fig.subplots_adjust(wspace=0, hspace=0)
+                mpl.rcParams['axes.linewidth'] = 4
 
                 optical_file = DATASET_PATH / 'sn7' / 'sentinel2' / f'sentinel2_{aoi_id}.tif'
                 plot_optical(axs[0], optical_file, vis='false_color', show_title=False)
@@ -204,9 +211,9 @@ def qualitative_testing_comparison(config_names: list, checkpoint: int):
 
 if __name__ == '__main__':
     config_name = 'baseline_fusion'
-    # qualitative_testing('baseline_fusion')
-    # quantitative_testing(config_name, save_output=True)
-
+    checkpoint = 100
+    # qualitative_testing('baseline_fusion', checkpoint)
+    # quantitative_testing(config_name, checkpoint, save_output=True)
     # plot_quantitative_testing(['baseline_sar', 'baseline_optical', 'baseline_fusion'], ['SAR', 'optical', 'fusion'])
-    qualitative_testing_comparison(['baseline_sar', 'baseline_optical', 'baseline_fusion'], 100)
+    qualitative_testing_comparison(['baseline_sar', 'baseline_optical', 'baseline_fusion'], checkpoint)
 

utils/metrics.py
@@ -41,7 +41,7 @@ class MultiThresholdMetric():
         # self._y_pred = self._y_pred[None, ...]  # [B, Thresh, C, ...]
         # self._y_true = self._y_true[None,:, None, ...] # [Thresh, B,  C, ...]
 
-    def add_sample(self, y_true:torch.Tensor, y_pred):
+    def add_sample(self, y_true: torch.Tensor, y_pred: torch.Tensor):
         y_true = y_true.bool()[None,...] # [Thresh, B,  C, ...]
         y_pred = y_pred[None, ...]  # [Thresh, B, C, ...]
         y_pred_offset = (y_pred - self._thresholds + 0.5).round().bool()
@@ -86,91 +86,34 @@ class MultiThresholdMetric():
         denom = (self.precision + self.recall).clamp(10e-05)
         return 2 * self.precision * self.recall / denom
 
-class MultiClassF1():
-    def __init__(self, ignore_last_class = False):
 
-        # FIXME Does not operate properly
-
-        '''
-        Takes in rasterized and batched images
-        :param y_true: [B, H, W]
-        :param y_pred: [B, C, H, W]
-        :param threshold: [Thresh]
-        '''
-
-        self._data_dims = (0, 2, 3) # For a B/W image, it should be [Thresh, B, C, H, W],
-        self.ignore_last_class = ignore_last_class
-        self.TP = 0
-        self.TN = 0
-        self.FP = 0
-        self.FN = 0
-
-    def add_sample(self, y_true:torch.Tensor, y_pred):
-        if self.ignore_last_class:
-            # Ignore background classes
-
-            y_pred = y_pred[:, :-1]
-            y_true = y_true[:, :-1]
-            y_true_mask = y_true[:, [-1]]
-
-
-        y_true = y_true.bool() # [B,  C, ...]
-        # Make y_pred one hot along dim C
-        y_pred_argmax = torch.argmax(y_pred, dim=1, keepdim=True)  # [B, C, ...]
-        y_pred = torch.zeros_like(y_pred, dtype=torch.bool).scatter_(1, y_pred_argmax,True)
-
-        self.TP += (y_true & y_pred).sum(dim=self._data_dims).float()
-        self.TN += ((~y_true & ~y_pred) * y_true_mask).sum(dim=self._data_dims).float()
-        self.FP += ((~y_true & y_pred) * y_true_mask).sum(dim=self._data_dims).float()
-        self.FN += (y_true & ~y_pred).sum(dim=self._data_dims).float()
-
-
-    def compute_basic_metrics(self):
-        '''
-        Computes False Negative Rate and False Positive rate
-        :return:
-        '''
-
-        false_pos_rate = self.FP/(self.FP + self.TN)
-        false_neg_rate = self.FN / (self.FN + self.TP)
-
-        return false_pos_rate, false_neg_rate
-
-    def compute_f1(self, include_bg=False):
-        self.TP = self.TP.clamp(10e-05)
-        self.TN = self.TN.clamp(10e-05)
-        self.FP = self.FP.clamp(10e-05)
-        self.FN = self.FN.clamp(10e-05)
-        individual_f1 = 2 * self.TP / (2 * self.TP + self.FN + self.FP)
-        if not include_bg:
-            individual_f1 = individual_f1[..., :-1]
-        f1 = len(individual_f1) / (individual_f1 ** -1).sum()
-
-        f1 = f1.cpu().item()
-        individual_f1 = individual_f1.cpu().numpy()
-        return f1, individual_f1
-
-def true_pos(y_true, y_pred, dim=0):
-    return torch.sum(y_true * torch.round(y_pred), dim=dim) # Only sum along H, W axis, assuming no C
+def true_pos(y_true: torch.Tensor, y_pred: torch.Tensor, dim=0):
+    return torch.sum(y_true * torch.round(y_pred), dim=dim)  # Only sum along H, W axis, assuming no C
 
 
 def false_pos(y_true, y_pred, dim=0):
-    return torch.sum(y_true * (1. - torch.round(y_pred)), dim=dim)
-
-
-def false_neg(y_true, y_pred, dim=0):
     return torch.sum((1. - y_true) * torch.round(y_pred), dim=dim)
 
 
-def precision(y_true, y_pred, dim):
-    denom = (true_pos(y_true, y_pred, dim) + false_pos(y_true, y_pred, dim))
+def false_neg(y_true: torch.Tensor, y_pred: torch.Tensor, dim=0):
+    return torch.sum(y_true * (1. - torch.round(y_pred)), dim=dim)
+
+
+def precision(y_true: torch.Tensor, y_pred: torch.Tensor, dim: int):
+    TP = true_pos(y_true, y_pred, dim)
+    FP = false_pos(y_true, y_pred, dim)
+    denom = TP + FP
+    denom = torch.clamp(denom, 10e-05)
+    return TP / denom
+
+
+def recall(y_true: torch.Tensor, y_pred: torch.Tensor, dim: int):
+    TP = true_pos(y_true, y_pred, dim)
+    FN = false_neg(y_true, y_pred, dim)
+    denom = TP + FN
     denom = torch.clamp(denom, 10e-05)
     return true_pos(y_true, y_pred, dim) / denom
 
-def recall(y_true, y_pred, dim):
-    denom = (true_pos(y_true, y_pred, dim) + false_neg(y_true, y_pred, dim))
-    denom = torch.clamp(denom, 10e-05)
-    return true_pos(y_true, y_pred, dim) / denom
 
 def f1_score(gts:torch.Tensor, preds:torch.Tensor, multi_threashold_mode=False, dim=(-1, -2)):
     # FIXME Does not operate proper
@@ -191,10 +134,14 @@ def f1_score(gts:torch.Tensor, preds:torch.Tensor, multi_threashold_mode=False, 
     return f1
 
 
-def roc_score(y_true:torch.Tensor, y_preds:torch.Tensor, ):
-    y_preds = y_preds.flatten().cpu().numpy()
-    y_true = y_true.flatten().cpu().numpy()
+if __name__ == '__main__':
+    import numpy as np
+    y_true = torch.Tensor(np.array([1, 1, 1, 1]))
+    y_pred = torch.Tensor(np.array([1, 0, 0, 0]))
+    print(f'TP {true_pos(y_true, y_pred)}')
+    print(f'FP {false_pos(y_true, y_pred)}')
+    print(f'FN {false_neg(y_true, y_pred)}')
+    print(f'precision {precision(y_true, y_pred, dim=0)}')
+    print(f'recall {recall(y_true, y_pred, dim=0)}')
+    print(f'f1 score {f1_score(y_true, y_pred, dim=0)}')
 
-    curve = roc_curve(y_true, y_preds, pos_label=1,  drop_intermediate=False)
-    # print(curve)
-    return curve

utils/visualization.py
@@ -6,14 +6,15 @@ import numpy as np
 from pathlib import Path
 
 
-def plot_optical(ax, file: Path, vis: str = 'true_color', scale_factor: float = 0.3,
+def plot_optical(ax, file: Path, vis: str = 'true_color', scale_factor: float = 0.4,
                  show_title: bool = False):
     img, _, _ = read_tif(file)
     band_indices = [2, 1, 0] if vis == 'true_color' else [6, 2, 1]
     bands = img[:, :, band_indices] / scale_factor
     bands = bands.clip(0, 1)
     ax.imshow(bands)
-    ax.set_axis_off()
+    ax.set_xticks([])
+    ax.set_yticks([])
     if show_title:
         ax.set_title(f'optical ({vis})')
 
@@ -24,7 +25,8 @@ def plot_sar(ax, file: Path, vis: str = 'VV', show_title: bool = False):
     bands = img[:, :, band_index]
     bands = bands.clip(0, 1)
     ax.imshow(bands, cmap='gray')
-    ax.set_axis_off()
+    ax.set_xticks([])
+    ax.set_yticks([])
     if show_title:
         ax.set_title(f'sar ({vis})')
 
@@ -37,14 +39,15 @@ def plot_buildings(ax, file: Path, show_title: bool = False):
     boundaries = [0, 0.5, 1]
     norm = colors.BoundaryNorm(boundaries, cmap.N, clip=True)
     ax.imshow(img, cmap=cmap, norm=norm)
-    ax.set_axis_off()
+    ax.set_xticks([])
+    ax.set_yticks([])
     if show_title:
         ax.set_title('ground truth')
 
 
 def plot_activation(ax, activation: np.ndarray, show_title: bool = False):
     ax.imshow(activation, cmap='jet', vmin=0, vmax=1)
-    ax.set_axis_off()
+    # ax.set_axis_off()
     if show_title:
         ax.set_title('activation')
 
@@ -54,6 +57,7 @@ def plot_prediction(ax, prediction: np.ndarray, show_title: bool = False):
     boundaries = [0, 0.5, 1]
     norm = colors.BoundaryNorm(boundaries, cmap.N, clip=True)
     ax.imshow(prediction, cmap=cmap, norm=norm)
-    ax.set_axis_off()
+    ax.set_xticks([])
+    ax.set_yticks([])
     if show_title:
         ax.set_title('prediction')
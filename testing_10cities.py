39061ec41beccd9b1dacda6303eefc57b11f708a
SebastianHafner <hafnersailing@gmail.com>
2020-11-16 19:49:40 +0100

    inference for entire sites

--- testing_10cities.py
+++ testing_10cities.py
@@ -1,24 +1,57 @@
-from utils.visualization import *
 from pathlib import Path
-import numpy as np
-import json
 import torch
-from tqdm import tqdm
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 from networks.network_loader import load_network
-from utils.dataloader import SpaceNet7Dataset
 from experiment_manager.config import config
-from utils.metrics import *
+from utils.dataloader import InferenceDataset, UrbanExtractionDataset
 from utils.geotiff import *
+from tqdm import tqdm
+import numpy as np
+import torchvision.transforms.functional as TF
 
-# TODO: add coordinates to title for area of interests
-
-URBAN_EXTRACTION_PATH = Path('/storage/shafner/urban_extraction')
-DATASET_PATH = Path('/storage/shafner/urban_extraction/urban_extraction_dataset_version4')
+ROOT_PATH = Path('/storage/shafner/urban_extraction')
 CONFIG_PATH = Path('/home/shafner/urban_dl/configs')
-NETWORK_PATH = Path('/storage/shafner/urban_extraction/networks/')\
+NETWORK_PATH = Path('/storage/shafner/urban_extraction/networks/')
 
 
 def run_inference(config_name: str, checkpoint: int, site: str):
-    pass
+    # loading config and network
+    cfg = config.load_cfg(CONFIG_PATH / f'{config_name}.yaml')
+    net_file = NETWORK_PATH / f'{config_name}_{checkpoint}.pkl'
+    net = load_network(cfg, net_file)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net.to(device)
+    net.eval()
+
+    # loading dataset from config (requires inference.json)
+    dataset = UrbanExtractionDataset(cfg, site, include_projection=True)
+
+    # config inference directory
+    save_path = ROOT_PATH / 'inference' / config_name
+    save_path.mkdir(exist_ok=True)
+    # site directory
+    save_path = save_path / site
+    save_path.mkdir(exist_ok=True)
+
+    with torch.no_grad():
+        for i in tqdm(range(len(dataset))):
+            patch = dataset.__getitem__(i)
+            img = patch['x'].to(device)
+            logits = net(img.unsqueeze(0))
+            prob = torch.sigmoid(logits)
+            pred = prob > cfg.THRESHOLDS.VALIDATION
+            pred = pred.squeeze().cpu().numpy().astype('uint8')
+
+            transform = patch['transform']
+            crs = patch['crs']
+            patch_id = patch['patch_id']
+            pred_file = save_path / f'pred_{config_name}_{patch_id}.tif'
+            write_tif(pred_file, pred, transform, crs)
+
+    combine_tif_patches(save_path, f'pred_{config_name}', delete_tiles=True)
+
+
+if __name__ == '__main__':
+    config_name = 'fusion'
+    checkpoint = 50
+    roi_id = 'milano'
+    run_inference(config_name, checkpoint, roi_id)
--- utils/geotiff.py
+++ utils/geotiff.py
@@ -1,6 +1,7 @@
 import rasterio
 import json
 from pathlib import Path
+import numpy as np
 
 
 # reading in geotiff file as numpy array
@@ -23,7 +24,12 @@ def write_tif(file: Path, arr, transform, crs):
     if not file.parent.exists():
         file.parent.mkdir()
 
-    height, width, bands = arr.shape
+    if len(arr.shape) == 3:
+        height, width, bands = arr.shape
+    else:
+        height, width = arr.shape
+        bands = 1
+        arr = arr[:, :, None]
     with rasterio.open(
             file,
             'w',
@@ -37,6 +43,58 @@ def write_tif(file: Path, arr, transform, crs):
     ) as dst:
         for i in range(bands):
             dst.write(arr[:, :, i], i + 1)
+
+
+def get_coords(file: Path):
+    file_parts = file.stem.split('_')
+    coord_part = file_parts[-1]
+    coords = coord_part.split('-')
+    coords = [int(coord) for coord in coords]
+    return coords
+
+
+def basename_from_file(file: Path):
+    file_parts = file.stem.split('_')
+    base_parts = file_parts[:-1]
+    base_name = '_'.join(base_parts)
+    return base_name
+
+
+def combine_tif_patches(folder: Path, basename: str, delete_tiles: bool = False, dtype=np.int8):
+    files = [f for f in folder.glob('**/*') if f.is_file() and basename_from_file(f) == basename]
+    coords = [get_coords(f) for f in files]
+    i_coords = [coord[0] for coord in coords]
+    j_coords = [coord[1] for coord in coords]
+
+    max_i = max(i_coords)
+    max_j = max(j_coords)
+
+    # TODO: handle if upper left file does not exist (random file can be chosen)
+    ul_file = folder / f'{basename}_{0:010d}-{0:010d}.tif'
+    ul_arr, transform, crs = read_tif(ul_file)
+    tile_height, tile_width, n_bands = ul_arr.shape
+    assert (tile_height == tile_width)
+    tile_size = tile_height
+
+    lr_file = folder / f'{basename}_{max_i:010d}-{max_j:010d}.tif'
+    lr_arr, _, _ = read_tif(lr_file)
+    lr_height, lr_width, _ = lr_arr.shape
+
+    mosaic_height = max_i + lr_height
+    mosaic_width = max_j + lr_width
+    mosaic = np.full((mosaic_height, mosaic_width, n_bands), fill_value=-1, dtype=dtype)
+
+    for index, file in enumerate(files):
+        tile, _, _ = read_tif(file)
+        i_start, j_start = get_coords(file)
+        i_end = i_start + tile_size
+        j_end = j_start + tile_size
+        mosaic[i_start:i_end, j_start:j_end, ] = tile
+        if delete_tiles:
+            file.unlink()
+
+    output_file = folder / f'{basename}.tif'
+    write_tif(output_file, mosaic, transform, crs)
 
 
 def load_json(file: Path):
